5af7c0de2231d4e250dbe69e17e27dc354f99c0c
renjieliu <anlrj@qq.com>
2022-05-15 18:17:30 +0000

    fib

---
+++ fib.py
@@ -0,0 +1,18 @@
+def fib(n):
+    if n <= 0:
+        
+        return -1
+    elif n < 3:
+        return 1
+    else:
+        a = b = 1 
+        for i in range(3, n+1):
+            c = a+b
+            a = b
+            b = c
+        return c
+
+
+print(fib(100))
+print(fib(-1))
+
